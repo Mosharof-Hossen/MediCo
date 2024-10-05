4e173bc5b03a80710e02d4d799bed27922f52b65
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-10-05 20:04:04 +0600

    secific category data shown

--- client/src/Components/AllCategories/CategoryCard.jsx
+++ client/src/Components/AllCategories/CategoryCard.jsx
@@ -8,12 +8,11 @@ const CategoryCard = ({ category }) => {
                 <figure>
                     <img
                         src={category.categoryImage}
-                        alt="Shoes" />
+                        alt="image" />
                 </figure>
                 <div className="card-body">
                     <h2 className="text-2xl font-bold text-center">{category.categoryName}</h2>
                     <p className='text-center text-xs text-gray-500'>{category.description}</p>
-
                 </div>
             </div>
         </Link>
--- client/src/Pages/Category/Category.jsx
+++ client/src/Pages/Category/Category.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import useFetchProductByCategory from '../../API/useFetchProductByCategory';
+import { FaEye } from 'react-icons/fa';
 
 const Category = () => {
     const category = useParams();
@@ -15,6 +16,45 @@ const Category = () => {
     return (
         <div>
 
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra">
+                        {/* head */}
+                        <thead>
+                            <tr className=''>
+                                <th></th>
+                                <th>Image</th>
+                                <th>Item Name</th>
+                                <th>Brand</th>
+                                <th>Unit Per Price</th>
+                                <th>View</th>
+                                <th>Add to Cart</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                products.map((item, i) => <tr key={item._id}>
+                                    <th>{i + 1}</th>
+                                    <th>
+                                        <div className="avatar">
+                                            <div className="w-14 rounded-xl">
+                                                <img src={item.image} />
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <td>{item.itemName}</td>
+                                    <td>{item.company}</td>
+                                    <td>$ {item.perUnitPrice}</td>
+                                    <td><button className='flex items-center '><FaEye className='text-2xl text-primary-c' /></button></td>
+                                    <td><button className='flex items-center bg-primary-c text-white px-3 py-2 rounded'>Add to Cart</button></td>
+                                </tr>)
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
