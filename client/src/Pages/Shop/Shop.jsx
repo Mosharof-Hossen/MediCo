3ded7f9b90e30529551f9b6dbc79577b82062161
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-10-07 06:31:24 +0600

    shop page design done

--- client/src/Pages/Shop/Shop.jsx
+++ client/src/Pages/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import useFetchGetItem from "../../API/useFetchGetItem";
 import ProductCart from "./ProductCart";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
 
 const Shop = () => {
@@ -13,12 +14,31 @@ const Shop = () => {
     console.log(data);
     return (
         <div>
+            <div className="drawer lg:drawer-open">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer-2" className="btn mr-auto m-5 drawer-button lg:hidden">
+                        Filter <FaAngleDoubleRight className="text-2xl"/>
+                    </label>
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5">
+                        {
+                            data.map((item) => <ProductCart key={item._id} item={item}></ProductCart>)
+                        }
+                    </div>
 
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5">
-                {
-                    data.map((item) => <ProductCart key={item._id} item={item}></ProductCart>)
-                }
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full lg:w-48 w-52 p-4 lg:mt-0 mt-16">
+                        {/* Sidebar content here */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
             </div>
+
+
         </div>
     );
 };
