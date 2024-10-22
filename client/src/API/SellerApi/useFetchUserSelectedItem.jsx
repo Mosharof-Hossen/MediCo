1e0b6afb091328227a4cf3903cc742913209da23
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-10-23 01:24:12 +0600

    table created by user selected items

--- client/src/API/SellerApi/useFetchUserSelectedItem.jsx
+++ client/src/API/SellerApi/useFetchUserSelectedItem.jsx
@@ -4,9 +4,8 @@ import { useQuery } from '@tanstack/react-query';
 const useFetchUserSelectedItem = (id) => {
     const axios = useAxios(); 
     const fetchItemById = async (ids) => {
-        console.log(ids);
         const res = id && await axios.get(`/user-selected-items/${ids}`);
-        return res.data;
+        return res.data[0];
     }
     return useQuery({
         queryKey: ['userSelectedItems', id],
--- client/src/Pages/Seller/UserSelectedItems/UserSelectedItems.jsx
+++ client/src/Pages/Seller/UserSelectedItems/UserSelectedItems.jsx
@@ -5,11 +5,54 @@ import useFetchUserSelectedItem from "../../../API/SellerApi/useFetchUserSelecte
 const UserSelectedItems = () => {
     const { id } = useParams();
 
-    const { data: items, isLoading, error } = useFetchUserSelectedItem(id);
+    const { data: items, isLoading, isError } = useFetchUserSelectedItem(id);
+    if (isLoading) {
+        return <div className='text-center'><span className='loading loading-bars loading-lg'></span></div>
+    }
+    if (isError) {
+        return
+    }
     console.log(items);
     return (
         <div>
-            <SectionTitle ></SectionTitle>
+            <SectionTitle heading={'User Selected Items'} subHeading={'Analyze the preferences of users based on the items they have selected. '}></SectionTitle>
+            <div className="bg-white p-5 rounded">
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra">
+                        {/* head */}
+                        <thead>
+                            <tr className='bg-primary-c text-white'>
+                                <th className="rounded-tl-3xl"></th>
+                                <th>Image</th>
+                                <th>Item Name</th>
+                                <th>Category</th>
+                                <th>Company</th>
+                                <th className="rounded-tr-3xl">Unit Per Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                items?.itemDetails?.map((item, i) => <tr key={item._id}>
+                                    <th>{i + 1}</th>
+                                    <th>
+                                        <div className="avatar">
+                                            <div className="w-14 rounded-xl">
+                                                <img src={item.image} />
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <td>{item.itemName}</td>
+                                    <td>{item.category}</td>
+                                    <td>{item.company}</td>
+                                    <td>{item.perUnitPrice}$</td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
 
         </div>
     );
