f5ba1aa3a00e4031cdc1a87973aa0a930aac9056
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-11-06 00:45:34 +0600

    short feature section added

--- client/src/Components/ShortFeature/ShortFeature.jsx
+++ client/src/Components/ShortFeature/ShortFeature.jsx
@@ -5,14 +5,14 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-// import './styles.css';
+import './shortFeature.css';
 
 // import required modules
 import { Pagination } from 'swiper/modules';
 
 const ShortFeature = () => {
     return (
-        <div>
+        <div className='my-10 px-5'>
             <Swiper
                 slidesPerView={3}
                 spaceBetween={30}
@@ -22,31 +22,49 @@ const ShortFeature = () => {
                 modules={[Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <div className='bg-green-200 w-full'>
-                        <h4>Health Care</h4>
-                        <p>Care Comes to You</p>
+                <SwiperSlide className='featureSlide'>
+                    <div className='bg-gradient-to-br from-green-50 to-green-400 rounded-xl h-full flex justify-center items-center w-full '>
+                        <div className='space-y-2'>
+                            <h4 className='font-bold text-xl'>Health Care</h4>
+                            <p className='text-sm text-gray-600'>Care Comes to You</p>
+                        </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <div className='bg-green-200 w-full'>
-                        <h4>Health Care</h4>
-                        <p>Care Comes to You</p>
+
+                <SwiperSlide className='featureSlide'>
+                    <div className='bg-gradient-to-br from-orange-50 to-orange-400 rounded-xl h-full flex justify-center items-center w-full '>
+                        <div className='space-y-2'>
+                            <h4 className='font-bold text-xl'>MediCo Beauty</h4>
+                            <p className='text-sm text-gray-600'>Glamour Delivered, Always Chic</p>
+                        </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <div className='bg-green-200 w-full'>
-                        <h4>Health Care</h4>
-                        <p>Care Comes to You</p>
+                <SwiperSlide className='featureSlide'>
+                    <div className='bg-gradient-to-br from-blue-50 to-blue-400 rounded-xl h-full flex justify-center items-center w-full '>
+                        <div className='space-y-2'>
+                            <h4 className='font-bold text-xl'>Lab Test</h4>
+                            <p className='text-sm text-gray-600'>Diagnosing Health, Delivering Hope</p>
+                        </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <div className='bg-green-200 w-full'>
-                        <h4>Health Care</h4>
-                        <p>Care Comes to You</p>
+                <SwiperSlide className='featureSlide'>
+                    <div className='bg-gradient-to-br from-yellow-50 to-yellow-400 rounded-xl h-full flex justify-center items-center w-full '>
+                        <div className='space-y-2'>
+                            <h4 className='font-bold text-xl'>Pet and Vet</h4>
+                            <p className='text-sm text-gray-600'>Your Pets joy non-stop</p>
+                        </div>
                     </div>
                 </SwiperSlide>
-                
+                <SwiperSlide className='featureSlide'>
+                    <div className='bg-gradient-to-br from-indigo-50 to-indigo-400 rounded-xl h-full flex justify-center items-center w-full '>
+                        <div className='space-y-2'>
+                            <h4 className='font-bold text-xl'>Food</h4>
+                            <p className='text-sm text-gray-600'>Best food for the health</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+
             </Swiper>
         </div>
     );
